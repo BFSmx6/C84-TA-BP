e421db65c9f2e0653de2daf252bcb3420ad7c060
deepali-m <dmokashi@asu.edu>
2022-02-05 00:01:58 +0530

    Update Search.js

--- screens/Search.js
+++ screens/Search.js
@@ -23,7 +23,9 @@ export default class SearchScreen extends Component {
   };
 
   getTransactions = () => {
-    db.collection("transactions").get().then(snapshot => {
+    db.collection("transactions")
+      .get()
+      .then(snapshot => {
         snapshot.docs.map(doc => {
           this.setState({
             allTransactions: [...this.state.allTransactions, doc.data()],
@@ -32,46 +34,19 @@ export default class SearchScreen extends Component {
       });
   };
 
-  handleSearch = async text => {
-    var enteredText = text.toUpperCase().split("");
-    text = text.toUpperCase();
-    this.setState({
-      allTransactions: []
-    });
-    if (!text) {
-      this.getTransactions();
-    }
-    if (enteredText[0] === "B") {
-      db.collection("transactions")
-        .where("book_id", "==", text)
-        .get()
-        .then(snapshot => {
-          snapshot.docs.map(doc => {
-            this.setState({
-              allTransactions: [...this.state.allTransactions, doc.data()],
-            
-            });
-          });
-        });
-    } else if (enteredText[0] === "S") {
-      db.collection("transactions")
-        .where("student_id", "==", text)
-        .get()
-        .then(snapshot => {
-          snapshot.docs.map(doc => {
-            this.setState({
-              allTransactions: [...this.state.allTransactions, doc.data()],
-             
-            });
-          });
-        });
-    }
-  };
+  
 
   
   renderItem = ({ item, i }) => {
-    var date = item.date.toDate().toString().split(" ") .splice(0, 4).join(" ");
-    var transactionType = item.transaction_type === "issue" ? "issued" : "returned";
+    var date = item.date
+      .toDate()
+      .toString()
+      .split(" ")
+      .splice(0, 4)
+      .join(" ");
+
+    var transactionType =
+      item.transaction_type === "issue" ? "issued" : "returned";
     return (
       <View style={{ borderWidth: 1 }}>
         <ListItem key={i} bottomDivider>
@@ -86,13 +61,29 @@ export default class SearchScreen extends Component {
             <View style={styles.lowerLeftContaiiner}>
               <View style={styles.transactionContainer}>
                 <Text
-                  style={[styles.transactionText, {color:item.transaction_type === "issue" ? "#78D304": "#0364F4" }]}
-                >{item.transaction_type.charAt(0).toUpperCase() + item.transaction_type.slice(1)}
+                  style={[
+                    styles.transactionText,
+                    {
+                      color:
+                        item.transaction_type === "issue"
+                          ? "#78D304"
+                          : "#0364F4"
+                    }
+                  ]}
+                >
+                  {item.transaction_type.charAt(0).toUpperCase() +
+                    item.transaction_type.slice(1)}
                 </Text>
                 <Icon
                   type={"ionicon"}
-                  name={ item.transaction_type === "issue" ? "checkmark-circle-outline": "arrow-redo-circle-outline"}
-                  color={item.transaction_type === "issue" ? "#78D304" : "#0364F4"}
+                  name={
+                    item.transaction_type === "issue"
+                      ? "checkmark-circle-outline"
+                      : "arrow-redo-circle-outline"
+                  }
+                  color={
+                    item.transaction_type === "issue" ? "#78D304" : "#0364F4"
+                  }
                 />
               </View>
               <Text style={styles.date}>{date}</Text>
@@ -102,18 +93,11 @@ export default class SearchScreen extends Component {
       </View>
     );
   };
+//complete the function handleSearch()
+  //handleSearch = async text => {
+    
 
-  handleSearch = async text => {
-    var enteredText = text.toUpperCase().split("");
-    text = text.toUpperCase();
-    this.setState({
-      allTransactions: []
-    });
-    if (!text) {
-      this.getTransactions();
-    }
-
-    if (enteredText[0] === "B") {
+    /*if (enteredText[0] === "B") {
       db.collection("transactions")
         .where("book_id", "==", text)
         .get()
@@ -121,26 +105,12 @@ export default class SearchScreen extends Component {
           snapshot.docs.map(doc => {
             this.setState({
               allTransactions: [...this.state.allTransactions, doc.data()],
-              lastVisibleTransaction: doc
             });
           });
         });
-    } else if (enteredText[0] === "S") {
-      db.collection("transactions")
-        .where("student_id", "==", text)
-        .get()
-        .then(snapshot => {
-          snapshot.docs.map(doc => {
-            this.setState({
-              allTransactions: [...this.state.allTransactions, doc.data()],
-              lastVisibleTransaction: doc
-            });
-          });
-        });
-    }
-  };
-
-
+    } */
+    
+  
   render() {
     const { searchText, allTransactions } = this.state;
     return (
@@ -155,13 +125,17 @@ export default class SearchScreen extends Component {
             />
             <TouchableOpacity
               style={styles.scanbutton}
-              onPress={() => this.handleSearch(searchText)}
             >
               <Text style={styles.scanbuttonText}>Search</Text>
             </TouchableOpacity>
           </View>
         </View>
         <View style={styles.lowerContainer}>
+          <FlatList
+            data={allTransactions}
+            renderItem={this.renderItem}
+            keyExtractor={(item, index) => index.toString()}
+          />
         </View>
       </View>
     );
@@ -194,7 +168,6 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     fontSize: 18,
     backgroundColor: "#5653D4",
-    fontFamily: "Rajdhani_600SemiBold",
     color: "#FFFFFF"
   },
   scanbutton: {
@@ -209,7 +182,6 @@ const styles = StyleSheet.create({
   scanbuttonText: {
     fontSize: 24,
     color: "#0A0101",
-    fontFamily: "Rajdhani_600SemiBold"
   },
   lowerContainer: {
     flex: 0.8,
@@ -217,11 +189,9 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20,
-    fontFamily: "Rajdhani_600SemiBold"
   },
   subtitle: {
     fontSize: 16,
-    fontFamily: "Rajdhani_600SemiBold"
   },
   lowerLeftContaiiner: {
     alignSelf: "flex-end",
@@ -235,11 +205,9 @@ const styles = StyleSheet.create({
   transactionText: {
     fontSize: 20,
 
-    fontFamily: "Rajdhani_600SemiBold"
   },
   date: {
     fontSize: 12,
-    fontFamily: "Rajdhani_600SemiBold",
     paddingTop: 5
   }
 });
